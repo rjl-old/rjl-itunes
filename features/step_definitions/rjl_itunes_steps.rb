7b28bb51696f7170d5c248f87a870265f2b91bc6
Richard Lyon <richard@richardlyon.net>
2016-02-07 10:24:53 +0000

    Initial commit

--- features/step_definitions/rjl_itunes_steps.rb
+++ features/step_definitions/rjl_itunes_steps.rb
@@ -1,3 +1,23 @@
 module KnowsTheDomain
+  def itunes
+    @itunes ||= Itunes.new
+  end
 end
 World(KnowsTheDomain)
+
+Given(/^there is an iTunes plist$/) do
+  itunes = Itunes.new
+  expect(itunes.itunes_path).to eq('/Users/richlyon/Music/iTunes/iTunes Music Library.xml')
+end
+
+When(/^I get the first album$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^it has the artist "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the album title is "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
