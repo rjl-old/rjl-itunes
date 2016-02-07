1d7cba2735f92217649b1801f44e95771e8595cb
Richard Lyon <richard@richardlyon.net>
2016-02-07 16:05:43 +0000

    album_id -> track_id
    Correct error

--- features/step_definitions/rjl_itunes_steps.rb
+++ features/step_definitions/rjl_itunes_steps.rb
@@ -5,14 +5,32 @@ module KnowsTheDomain
 end
 World(KnowsTheDomain)
 
-Given(/^album (\d+)$/) do |album_id|
+# Scenario Outline: Get artist group and genre information
+
+Given(/^track (\d+)$/) do |track_id|
   itunes = Itunes.new
-  $album = itunes.album( album_id )
+  $track = itunes.album( track_id )
 end
 
 Then(/^artist "([^"]*)", album "([^"]*)", grouping "([^"]*)", genre "([^"]*)"$/) do |artist, album, grouping, genre|
-  expect($album["Artist"]).to eql(artist)
-  expect($album["Album"]).to eql(album)
-  expect($album["Grouping"]).to eql(grouping)
-  expect($album["Genre"]).to eql(genre)
+  expect($track["Artist"]).to eql(artist)
+  expect($track["Album"]).to eql(album)
+  expect($track["Grouping"]).to eql(grouping)
+  expect($track["Genre"]).to eql(genre)
+end
+
+# Scenario: Change group and genre information without saving to disk
+
+Given(/^track with id (\d+)$/) do |track_id|
+  $track_id = track_id
+  $track = itunes.album( track_id )
+end
+
+
+When(/^I change the grouping to "([^"]*)"$/) do |new_grouping|
+  itunes.update_album( $track_id, "Grouping", new_grouping)
+end
+
+Then(/^all the album track groupings are "([^"]*)"$/) do |grouping|
+  expect(itunes.same?( $track_id, grouping)).to be_truthy
 end
