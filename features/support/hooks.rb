16a1599db17abc5c14b6b68de6f1d7947626f620
richardjlyon <richard@richardlyon.net>
2016-02-08 22:19:38 +0000

    Add test xml data and hook to reset

---
+++ features/support/hooks.rb
@@ -0,0 +1,9 @@
+CLEAN = '/Users/richlyon/Coding/Ruby/development/rjl_itunes/features/assets/clean/iTunes Music Library.xml'
+TEST = '/Users/richlyon/Coding/Ruby/development/rjl_itunes/features/assets/test/iTunes Music Library.xml'
+Before do
+  puts "Copied clean iTunes Music Library.xml into test directory"
+  FileUtils.cp_r CLEAN, TEST
+end
+
+After do
+end
