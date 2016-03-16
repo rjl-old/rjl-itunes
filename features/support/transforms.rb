c218646ce2084f37c2a68a7768b67a68ac2483cf
richardjlyon <richard@richardlyon.net>
2016-03-16 09:29:55 +0000

    Deleted

--- features/support/transforms.rb
+++
@@ -1,3 +0,0 @@
-CAPTURE_TRUE_FALSE = Transform /(True|true|False|false)/ do |state|
-  state == "true" || state == "True"
-end
--- features/support/world_extension.rb
+++
@@ -1,7 +0,0 @@
-module KnowsTheDomain
-  def itunes
-    @itunes ||= Itunes.new
-  end
-end
-
-World(KnowsTheDomain)
