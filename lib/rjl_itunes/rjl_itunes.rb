306bd5b7032d306c60398bfe18fc720380d853e0
richardjlyon <richard@richardlyon.net>
2016-03-17 10:41:27 +0000

    Deletions

--- lib/rjl_itunes/rjl_itunes.rb
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-
-class Rjl_itunes
-
-  def initialize
-    puts "class Rjl_itunes initialised"
-  end
-end
--- lib/rjl_itunes/version.rb
+++
@@ -1,5 +0,0 @@
-# coding: utf-8
-
-class Rjl_itunes
-  VERSION = "0.1"
-end
