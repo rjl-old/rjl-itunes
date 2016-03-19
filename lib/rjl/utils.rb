f94409d573864d6c4f8ff95bce42228964efb6fb
richardjlyon <richard@richardlyon.net>
2016-03-19 17:28:03 +0000

    Add logging

--- lib/rjl/utils.rb
+++ lib/rjl/utils.rb
@@ -1,3 +1,5 @@
+require 'logger'
+
 # return true if tracks have only one value for 'parameter_name'
 def unique?( tracks, parameter_name )
   values = []
@@ -5,4 +7,13 @@ def unique?( tracks, parameter_name )
     values << track.send( parameter_name )
   end
   return values.uniq.length == 1
+end
+
+
+# a logger
+def get_logger
+  log_path = File.join((File.dirname __dir__), 'logs/log.log')
+  logger = Logger.new(log_path, 'daily')
+  logger.datetime_format = "%Y-%m-%d"
+  return logger
 end
