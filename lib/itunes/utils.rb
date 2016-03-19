d8e0515b2b084d31c48dababb331f02e6d529205
richardjlyon <richard@richardlyon.net>
2016-03-19 11:47:06 +0000

    Delete / rename

--- lib/itunes/utils.rb
+++
@@ -1,8 +0,0 @@
-# return true if tracks have only one value for 'parameter_name'
-def unique?( tracks, parameter_name )
-  values = []
-  tracks.each do |track|
-    values << track.send( parameter_name )
-  end
-  return values.uniq.length == 1
-end
